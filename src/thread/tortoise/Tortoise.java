25ba7a663e4d827d140659821f1bb95c0381ad0a
kinjal varshney <kinjal.varshney_cs19@gla.ac.in>
2020-10-29 18:50:53 +0530

    pause the thread for 3.0 seconds using the thread.sleep() method

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -1,5 +1,7 @@
 package thread.tortoise;
 
+import javax.swing.*;
+
 public class Tortoise extends Thread {
 
     private static final int MILESTONES = 5;
@@ -28,6 +30,17 @@ public class Tortoise extends Thread {
                     System.out.println("The Tortoise has reached the finish line!");
                     break;
             }
+            try {
+                Thread.sleep(3000L);
+            } catch (InterruptedException e) {
+                System.err.println("The Tortoise has lost the path!");
+            }
         }
+        JOptionPane.showConfirmDialog(
+                null,
+                "The Tortoise has completed the race!",
+                "Tortoise",
+                JOptionPane.OK_CANCEL_OPTION
+        );
     }
 }
